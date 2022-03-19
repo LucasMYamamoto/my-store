9679f370302fb779d8f24ad4431919220d792173
Pigture It <75996723+LucasMYamamoto@users.noreply.github.com>
2022-03-18 22:47:54 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Product} from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Lucas';
+  age= 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled =true;
+  person = {
+    name: 'Lucas',
+    age: 36,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  names: string[] = ['Lucas', 'Lupe', "Benja"];
+  newName='';
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  toggleButton(){
+    this.btnDisabled= !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    element.value;
+    this.person.name = element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName ='';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index,1);
+  }
+
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,19 @@
+const username: string = 'Lucas';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+sum(1, 2);
+
+
+class Person {
+
+
+  constructor(public age: number,public lastName:string){
+
+  }
+}
+
+const lucas = new Person(36,'Yamamoto');
+
